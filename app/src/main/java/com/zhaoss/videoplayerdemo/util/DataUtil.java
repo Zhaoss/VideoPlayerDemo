4f91967a7102be4d96e166d7f28b1bd1ad55d960
zhaoshuang <zhaoshuang@tataufo>
2019-11-13 18:24:51 +0800

    v2.1
    v2.1

--- app/src/main/java/com/zhaoss/videoplayerdemo/util/DataUtil.java
+++ app/src/main/java/com/zhaoss/videoplayerdemo/util/DataUtil.java
@@ -22,7 +22,7 @@ public class DataUtil {
                 "在即将正式发布的 Android P 版本中，谷歌彻底取消了对Apache HTTPClient的支持, 谷歌就推荐在Android2.3版本以上使用HttpURLConnection",
                 "https://oimryzjfe.qnssl.com/content/2EF3AE01AC1D36BDF42EFA87C275BB66.jpg"));
         mainVideoBeanList.add(new MainVideoBean(R.mipmap.avatar1,
-                "https://v2.xw0371.com/20180401/wiyCDyE3/index.m3u8",
+                "https://oimryzjfe.qnssl.com/content/fe9cfd1402bb40490bc9a208db7c0921.mp4",
                 "程序员小陈2",
                 "在即将正式发布的 Android P 版本中，谷歌彻底取消了对Apache HTTPClient的支持, 谷歌就推荐在Android2.3版本以上使用HttpURLConnection",
                 "https://oimryzjfe.qnssl.com/content/022294301d2d7da585eae4bbbe3653f6.png"));
